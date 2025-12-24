cd4116c505e8f97a91ab4056d75a13de7bd3fcf0
Fernanda Caleffi Catalani <fernanda.caleffi99@gmail.com>
2025-12-23 22:24:34 -0300

    refactor: use middleware for auth routing

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,13 +1,8 @@
-import { redirect } from "next/navigation";
 import { getServerSession } from "@/lib/get-server-session";
 import DashboardClient from "./DashboardClient";
 
 export default async function DashboardPage() {
   const user = await getServerSession();
-
-  if (!user) {
-    redirect("/sign-in");
-  }
 
   return <DashboardClient user={user!} />;
 }
