a54472638a59fba5ce94a1235707c973dbb72e42
Fernanda Caleffi Catalani <fernanda.caleffi99@gmail.com>
2026-02-12 15:55:43 -0300

    chore(msw): remove browser mocks and msw config

--- src/mocks/browser.ts
+++
@@ -1,5 +0,0 @@
-import { setupWorker } from "msw/browser";
-import { authHandlers } from "./handlers/auth";
-import { financesHandlers } from "./handlers/finances";
-
-export const worker = setupWorker(...authHandlers, ...financesHandlers);
